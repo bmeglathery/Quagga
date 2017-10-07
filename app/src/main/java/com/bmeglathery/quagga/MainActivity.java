48e5695c91e878a1232eaaac9a51736dfb6e6855
Brandon Meglathery <bsm29471@huskies.bloomu.edu>
2017-10-07 09:59:41 -0400

    Cleaned up some spacing, deleted unnecessary/commented lines of code

--- app/src/main/java/com/bmeglathery/quagga/MainActivity.java
+++ app/src/main/java/com/bmeglathery/quagga/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,8 +25,6 @@ import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
-
-import static android.graphics.Color.WHITE;
 
 /**
  * Plays the game of Quagga.
@@ -151,13 +148,11 @@ public class MainActivity extends AppCompatActivity {
             int imageId = getResources().getIdentifier(imageName, "drawable", getPackageName());
             imageView[i].setImageResource(imageId);
             imageView[i].startAnimation(shakeAnimation);
-            //Uncomment after correcting the .mp3 file
             diceRollAudioPlayer.start();
         }
 
         Context context = getApplicationContext();
         Toast toast = Toast.makeText(context, rollResult, Toast.LENGTH_LONG);
-        //toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
 
